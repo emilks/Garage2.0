3f500cf2ec9bf6d5797bbf6c962acfdefedf6d48
Klara Höglund <klarahoglund@hotmail.com>
2022-07-08 14:59:41 +0200

    First changes

--- Garage2.0/Program.cs
+++ Garage2.0/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=ParkedVehicles}/{action=Index}/{id?}");
+    pattern: "{controller=ParkedVehicles}/{action=Create}/{id?}");
 
 app.Run();

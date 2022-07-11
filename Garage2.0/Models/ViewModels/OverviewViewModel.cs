c3f3b0104ecac07bad73626978183320fe7b0b04
Klara Höglund <klarahoglund@hotmail.com>
2022-07-11 10:19:55 +0200

    Med VM

--- Garage2.0/Models/ViewModels/OverviewViewModel.cs
+++ Garage2.0/Models/ViewModels/OverviewViewModel.cs
@@ -11,5 +11,6 @@ namespace Garage2._0.Models.ViewModel
         public string RegNr { get; set; }
         [Display(Name = "Arrival Time")]
         public DateTime ArrivalTime { get; set; }
+        public int Id { get; internal set; }
     }
 }

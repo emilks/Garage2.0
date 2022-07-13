5c585bcc9e87d63f7efefdd72614533d78b56435
emilk <emilks@gmail.com>
2022-07-13 11:26:56 +0200

    Receipt Fix

--- Garage2.0/Models/ReceiptViewModel.cs
+++ Garage2.0/Models/ReceiptViewModel.cs
@@ -10,10 +10,13 @@ namespace Garage2._0.Models
         [Display(Name="Registration Number")]
         public string RegNr { get; set; }
 
+        [Display(Name = "Arrival Time")]
         public DateTime ArrivalTime { get; set; }
 
+        [Display(Name = "Leaving Time")]
         public DateTime LeaveTime { get; set; }
 
+        [Display(Name = "Time Parked")]
         public double TimeParked { get; set; }
 
         public double Price { get; set; }

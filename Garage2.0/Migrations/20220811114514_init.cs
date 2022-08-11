28f033ff071655067f6e23414ff326469d6362e5
Klara Höglund <klarahoglund@hotmail.com>
2022-08-11 13:52:03 +0200

    inget

---
+++ Garage2.0/Migrations/20220811114514_init.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage2._0.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ParkingSpace_Park_ParkId",
+                table: "ParkingSpace");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParkId",
+                table: "ParkingSpace",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "ParkingSpace",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ParkId",
+                value: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ParkingSpace_Park_ParkId",
+                table: "ParkingSpace",
+                column: "ParkId",
+                principalTable: "Park",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ParkingSpace_Park_ParkId",
+                table: "ParkingSpace");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParkId",
+                table: "ParkingSpace",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "ParkingSpace",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ParkId",
+                value: null);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ParkingSpace_Park_ParkId",
+                table: "ParkingSpace",
+                column: "ParkId",
+                principalTable: "Park",
+                principalColumn: "Id");
+        }
+    }
+}

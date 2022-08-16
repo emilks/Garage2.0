b08fcaa11a7dfa7f8fb40f091cb1362babe6753c
marlinshenoda <105081672+marlinshenoda@users.noreply.github.com>
2022-08-16 12:09:35 +0200

    migration

--- Garage2.0/Migrations/20220816100730_data.cs
+++ Garage2.0/Migrations/20220816100730_data.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Garage2._0.Migrations
 {
-    public partial class db : Migration
+    public partial class data : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
